b36aa9099a374cf99db52446bf44ea3264748fa9
Digambar Hawaldar <109816894+Digambarh100@users.noreply.github.com>
2022-11-03 16:22:24 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+package AvgDetails;
+
+public class Calculator {
+
+public float num1;
+public float num2;
+public float  num3;
+public double num4;
+public double num5;
+public double num6;
+public double Sum1;
+public double Sum2;
+public double Avg1;
+public double Avg2;
+
+public void displayAvg1Details() {
+	
+	System.out.println("Displaying Avg details \n***********");
+	System.out.println("Avg of 12,8,15 : " + Avg1);
+	System.out.println();
+	}
+public void displayAvg2Details() {
+	
+	System.out.println("Displaying Avg details \n***********");
+	System.out.println("Avg of 10,20,30 : " + Avg2);
+	System.out.println();
+	
+	
+	}
+
+}
